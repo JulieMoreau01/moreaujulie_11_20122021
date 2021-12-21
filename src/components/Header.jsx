ea812dd02dff8e00be19ec8b03e8b4a2d19714b7
Julie Moreau <julie_bonnet_01@hotmail.com>
2021-12-21 10:59:07 +0100

    First Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">Je suis le header</header>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home/Index.jsx
@@ -0,0 +1,7 @@
+import '../../App.css'
+
+function Home() {
+  return <p>Je suis la Home</p>
+}
+
+export default Home
